060e51c29e80416325a40230a88928dd2c0576bf
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-09 20:01:13 +0200

    first commit

---
+++ src/routes/PublicRoutes.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import Root from "../layouts/Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    // errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
